8a015cde3d32ada9682dd1d557d975d327120dab
Kinosuke <kinosuke@openreach.tech>
2024-10-04 00:07:33 +0700

    Add routes and connect database into app file

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ const compression = require('compression')
 const express = require('express')
 const { default: helmet } = require('helmet')
 const morgan = require('morgan')
+require('dotenv').config()
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(compression())
+
+require('./dbs/init.mongodb')
+
+app.use('/', require('./routes'))
 
 module.exports = app
